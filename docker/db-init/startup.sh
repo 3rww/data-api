0a590e956cde5897e795857496dbb1e19fdc558b
Christian Gass <christian.gass@civicmapper.com>
2021-06-22 17:37:09 -0400

    Multi-db config for docker

--- docker/db-init/startup.sh
+++ docker/db-init/startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 python3 manage.py migrate --no-input --database=default
-python3 manage.py migrate --no-input --database=rainfall_db
 # pipenv run python3 manage.py migrate --no-input
