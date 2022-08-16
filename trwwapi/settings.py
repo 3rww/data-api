5460b7c5bbee7860ca0747cd311f6b970d76562a
gassc <christiangass@outlook.com>
2022-08-16 11:00:57 -0400

    temp disable staticfiles_storage

--- trwwapi/settings.py
+++ trwwapi/settings.py
@@ -190,7 +190,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# NOTE: this is disabled due to this issue: https://github.com/encode/django-rest-framework/issues/8608
+# with Django 4.1, Whitenoise, and DRF
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
